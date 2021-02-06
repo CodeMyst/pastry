281549e4e0838cd05c2b191ef4d91843fd04958e
CodeMyst <code@myst.rs>
2021-02-06 22:19:16 +0100

    basic single file upload

--- source/app.d
+++ source/app.d
@@ -1,6 +1,22 @@
 import std.stdio;
+import std.getopt;
+import std.file;
+import std.path;
+import pastemyst;
 
-void main()
+void main(string[] args)
 {
-	writeln("Edit source/app.d to start your project.");
+    string filePath = args[1];
+
+    string contents = readText(filePath);
+    string ext = extension(filePath)[1..$];
+
+    string lang = getLanguageByExtension(ext).get().name;
+
+    const pasty = PastyCreateInfo(baseName(filePath), lang, contents);
+    const createInfo = PasteCreateInfo("", ExpiresIn.never, false, false, "", [pasty]);
+
+    const res = createPaste(createInfo);
+
+    writeln("https://paste.myst.rs/" ~ res.id);
 }
