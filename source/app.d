13e6c7d9834a2a04a4eae3683be813f6637d02df
CodeMyst <code@myst.rs>
2021-02-06 22:30:57 +0100

    multi file upload

--- source/app.d
+++ source/app.d
@@ -6,24 +6,30 @@ import pastemyst;
 
 void main(string[] args)
 {
-    string filePath = args[1];
+    PastyCreateInfo[] pasties;
 
-    string contents = readText(filePath);
-    string ext = extension(filePath);
-
-    string lang;
-
-    if (ext.length > 1)
+    foreach (arg; args[1..$])
     {
-        lang = getLanguageByExtension(ext[1..$]).get().name;
-    }
-    else
-    {
-        lang = "plain text";
+        string filePath = arg;
+
+        string contents = readText(filePath);
+        string ext = extension(filePath);
+
+        string lang;
+
+        if (ext.length > 1)
+        {
+            lang = getLanguageByExtension(ext[1..$]).get().name;
+        }
+        else
+        {
+            lang = "plain text";
+        }
+
+        pasties ~= PastyCreateInfo(baseName(filePath), lang, contents);
     }
 
-    const pasty = PastyCreateInfo(baseName(filePath), lang, contents);
-    const createInfo = PasteCreateInfo("", ExpiresIn.never, false, false, "", [pasty]);
+    const createInfo = PasteCreateInfo("", ExpiresIn.never, false, false, "", pasties);
 
     const res = createPaste(createInfo);
 
